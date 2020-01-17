670fe5fdab9d68f5c7bdea7bce5b3d90293a3d85
Jip Fr <jipfrijlink@gmail.com>
2020-01-17 18:12:34 +0100

    Update spacing

--- index.js
+++ index.js
@@ -48,6 +48,6 @@ async function subSearch(id, query) {
 
 }
 
-subSearch("tt7203552", "Not you").then(console.log); 				// The morning show				
-subSearch("tt1839578", "the government has").then(console.log); 	// Person of Interest
-subSearch("tt9850952", "English").then(console.log); 				// Medical Police
+subSearch("tt7203552", "Not you").then(console.log);            // The morning show				
+subSearch("tt1839578", "the government has").then(console.log); // Person of Interest
+subSearch("tt9850952", "English").then(console.log);            // Medical Police
