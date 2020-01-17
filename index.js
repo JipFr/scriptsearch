a33f6071f47326554a9b06202ed728fc00e90db3
Jip Fr <jipfrijlink@gmail.com>
2020-01-17 18:45:21 +0100

    Final commit, lol

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ const subSearch = require("./subSearch");
 subSearch("tt7203552", "Not you").then(console.log);            // The morning show				
 subSearch("tt1839578", "the government has").then(console.log); // Person of Interest
 subSearch("tt9850952", "English").then(console.log);            // Medical Police
+subSearch("tt4158110", "This can't be happening").then(console.log); // Mr. Robot
