067a6d0fa9ae5442bdbc3fc673072dc66121e6cf
Jip Frijlink <jip@air-van-meike.home>
2020-01-17 18:09:02 +0100

    Initial commit

---
+++ find_in_subs.js
@@ -0,0 +1,55 @@
+module.exports = (data, query) => {
+	query = query.trim().toLowerCase();
+	
+	let results = [];
+
+	for(let season in data) {
+		for(let episode in data[season]) {
+		
+			let sub = data[season][episode];
+
+			let lines = sub.split("\n\n");
+			
+			lines.forEach(line => {
+				
+				line = line.split("\n");
+				let times = line.splice(0, 1)[0];
+				let sentence = line.join(" \n");
+				line = line.join(" ");
+				
+				if(clearup(sentence).includes(clearup(query))) {
+					results.push({
+						line,
+						times,
+						meta: {
+							season: Number(season),
+							episode: Number(episode)
+						}
+					});
+				}
+
+			});
+
+		}
+	}
+
+	return {
+		data: results,
+		query
+	}
+
+}
+
+function clearup(str) {
+	str = str.toLowerCase();
+	let allowed = " abcdefghijklmnopqrstuvwxyz";
+
+	// let cleared = str.split("").filter(char => allowed.includes(char)).join("");
+	let cleared = "";
+	str.split("").forEach(char => {
+		if(allowed.includes(char)) {
+			cleared += char;
+		}
+	});
+	return cleared;
+}
---
+++ get_subtitle.js
@@ -0,0 +1,44 @@
+
+require("dotenv").config();
+
+const fetch = require("node-fetch");
+const fs = require("fs");
+
+const OpenSubtitles = require("opensubtitles-api");
+const OS = new OpenSubtitles({
+	useragent: process.env.os_useragent,
+	username: process.env.os_username,
+	password: process.env.os_password
+});
+
+module.exports = async (id, season, episode) => {
+
+	let file_name = get_file_name(id, season, episode);
+	let path = `subs/${file_name}`;
+
+	if(fs.existsSync(path)) {
+
+		let file = fs.readFileSync(path, "utf-8");
+		return file;
+
+	} else {
+		let results = await OS.search({
+			sublanguageid: "eng",
+			imdbid: id,
+			season,
+			episode,
+			gzip: false
+		});
+		let sub_res = await fetch(results.en.vtt);
+		let sub = await sub_res.text();
+
+		fs.writeFileSync(path, sub);
+		
+		return sub;
+	}
+
+}
+
+function get_file_name(id, season, episode) {
+	return `${id}${season ? `-${season.toString().padStart(2, 0)}-${episode.toString().padStart(2, 0)}` : ""}.txt`;
+}
