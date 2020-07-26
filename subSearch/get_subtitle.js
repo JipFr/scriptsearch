9b7d9d113832b783375d2dc6e467ed2530e4ea7d
Jip Fr <jipfrijlink@gmail.com>
2020-07-26 16:51:34 +0200

    Improve logging

--- subSearch/get_subtitle.js
+++ subSearch/get_subtitle.js
@@ -1,5 +1,6 @@
 
 require("dotenv").config();
+require("colors");
 
 const fetch = require("node-fetch");
 const fs = require("fs");
@@ -16,13 +17,16 @@ module.exports = async (id, season, episode) => {
 	let file_name = get_file_name(id, season, episode);
 	let path = `${__dirname}/subs/${file_name}`;
 
+	console.log(`Starting S${season} E${episode} of ${id}`.bold.gray);
+
 	if(fs.existsSync(path)) {
 
-        console.log(`Doing S${season} E${episode} of ${id}`);
+        console.log(`Local S${season} E${episode} of ${id}`.bold.yellow);
 		let file = fs.readFileSync(path, "utf-8");
 		return file;
 
 	} else {
+		console.log(`Fetching S${season} E${episode} of ${id}`.bold.green);
 		let results = await OS.search({
 			sublanguageid: "eng",
 			imdbid: id,
@@ -34,13 +38,19 @@ module.exports = async (id, season, episode) => {
         
         if(results.en && results.en.vtt) {
             let sub_res = await fetch(results.en.vtt);
-            let sub = await sub_res.text();
-
-            fs.writeFileSync(path, sub);
-            console.log(`Doing S${season} E${episode} of ${id}`);
+			let sub = await sub_res.text();
+			
+			if(!sub.includes("Sorry, maximum download count for IP")) {
+				fs.writeFileSync(path, sub);
+            	console.log(`Saved S${season} E${episode} of ${id}`.bold.green);
     		
-    		return sub;
+    			return sub;
+			} else {
+				console.log("Rate limited".bold.red);
+				return null;
+			}
         } else {
+			console.log(JSON.stringify(results, null, "\t".bold.red));
             return null;
         }
 		
