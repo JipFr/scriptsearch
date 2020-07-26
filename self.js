023b945a0e61bb88ae37c9990ba46543f0facd24
Jip Fr <jipfrijlink@gmail.com>
2020-07-26 19:10:08 +0200

    More logging!

--- self.js
+++ self.js
@@ -2,9 +2,13 @@
 const fs = require("fs");
 const subSearch = require("./subSearch");
 
+console.log("Imported modules, creating store");
+
 const store = d => {
 	console.log(d);
 	fs.writeFileSync(`results/${d.query}.json`, JSON.stringify(d, null, "\t"));
 };
 
+console.log("Starting search");
 subSearch("tt0056751", "Doctor Who").then(store);
+console.log("Started search");
--- subSearch/index.js
+++ subSearch/index.js
@@ -3,9 +3,11 @@ const get_subtitles = require("./get_subtitle");
 const find_in_subs = require("./find_in_subs");
 
 const fetch = require("node-fetch");
+require("colors");
 const { JSDOM } = require("jsdom");
 
 async function fetchData(id, season = 1, seasons = {}) {
+	console.log(`Getting season data for season ${season} of ${id}`.bold.green);
 	let res = await fetch(`https://www.imdb.com/title/${id}/episodes?season=${season}`)
 	let d = await res.text();
 
@@ -19,7 +21,7 @@ async function fetchData(id, season = 1, seasons = {}) {
 
 	let ep_links = [...document.querySelectorAll(".info")];
     seasons[season] = ep_links.length;
-    console.log("Season", season, "of", id, "has", ep_links.length, "episodes");
+    console.log("Season", season, "of", id, "has", ep_links.length, "episodes".bold.gray);
 
 	return seasons;
 
@@ -36,6 +38,7 @@ async function subSearch(id, query) {
 		let season = entry[0];
 		let episodes = entry[1];
 		for(let episode = 1; episode <= episodes; episode++) {
+			console.log(`Getting S${season} E${episode} of ${id}`.bold.green);
 			let sub = await get_subtitles(id, season, episode);
 			
 			subs[season.toString()][episode] = sub;
