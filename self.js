9e9dbd0edc5da41ca758d093d9eff32c49437626
Jip Fr <jipfrijlink@gmail.com>
2020-07-26 18:12:26 +0200

    Correct mistake

--- self.js
+++ self.js
@@ -2,10 +2,9 @@
 const fs = require("fs");
 const subSearch = require("./subSearch");
 
-subSearch("tt0056751", "Doctor Who").then(store);
-
-
 const store = d => {
 	console.log(d);
 	fs.writeFileSync(`results/${d.query}.json`, JSON.stringify(d, null, "\t"));
 };
+
+subSearch("tt0056751", "Doctor Who").then(store);
